2db828d09867fab216e356805e2d9aaf88d2226e
Jeffrey Stocker <kagesennin@gmail.com>
2018-08-08 11:35:11 -0700

    set DM to have a load screen

--- src/components/DM.js
+++ src/components/DM.js
@@ -1,13 +1,32 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 import Map from './map/map';
 import DmMenu from './DmMenu';
-
+import LoadNewMap from './Upload';
+import IO from 'socket.io/lib/client';
+import autobind from '../utilties';
 
 export default class DM extends Component {
   constructor (props) {
     super(props);
     this.state = {
+      showMapLoadScreen: false,
+      mapLocation: null,
+    }
+    autobind.call(this, this.toggleShowMapLoadScreen, this.setMap)
+    // this.toggleShowMapLoadScreen = this.toggleShowMapLoadScreen.bind(this);
+  }
 
+  setMap (mapLocation) {
+    this.setState({mapLocation})
+  }
+
+  toggleShowMapLoadScreen (state) {
+    if (state === undefined) {
+      this.setState({showMapLoadScreen: !this.state.showMapLoadScreen});
+    } else if (!!state) {
+      this.setState({showMapLoadScreen: true});
+    } else {
+      this.setState({showMapLoadScreen: false});
     }
   }
 
@@ -15,9 +34,9 @@ export default class DM extends Component {
     return (
       <div>
         DM
-
-        <DmMenu />
-        <Map />
+        {this.state.showMapLoadScreen && <LoadNewMap toggleShowMapLoadScreen={this.toggleShowMapLoadScreen} setMap={this.setMap} />}
+        <DmMenu toggleShowMapLoadScreen={this.toggleShowMapLoadScreen}/>
+        <Map mapLocation={this.state.mapLocation} />
       </div>
     )
   }
