eb77682be29f0565129f56ec7a53dad3986f1771
Jeffrey Stocker <kagesennin@gmail.com>
2018-08-08 11:34:10 -0700

    change DmMenu to a horizonal layout

--- src/components/DmMenu.js
+++ src/components/DmMenu.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default function DmMenu(props) {
   return (
-    <div className="LeftMenu verticalDisplay">
-      <button>Set Map</button>
+    <div className=" ">
+      <button onClick={() => {props.toggleShowMapLoadScreen(true)}}>Set Map</button>
       <button>Hide Map</button>
       <button>Show Map</button>
     </div>
   )
 }
+
+// verticalDisplay LeftMenu
